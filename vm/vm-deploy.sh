85ef5864142f7668a113e77ebb8a0edf6e5218f7
riverwolf67 <riverwolf67@gmail.com>
2025-02-17 20:34:02 -0600

    Update vm-deploy.sh

--- vm/vm-deploy.sh
+++ vm/vm-deploy.sh
@@ -4,7 +4,7 @@ VM_TYPE=${1:-"debian"} && echo "VM_TYPE $VM_OS"
 VMID=$(pvesh get /cluster/nextid) && echo "VMID $VMID"
 source /dev/stdin <<< "$(wget -qLO - https://raw.githubusercontent.com/riverwolf67/proxmox-automation-scripts/main/vm/vm-functions.sh)"
 
-bash -c "$(wget -qLO - https://github.com/community-scripts/ProxmoxVE/raw/main/vm/debian-vm.sh)"
+# bash -c "$(wget -qLO - https://github.com/community-scripts/ProxmoxVE/raw/main/vm/debian-vm.sh)"
 bash -c "$(wget -qLO - https://github.com/riverwolf67/proxmox-automation-scripts/raw/main/vm/debian-up.sh)"
 
 echo "Verify functions sourced correctly..."
